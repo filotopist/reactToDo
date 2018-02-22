6adfc925a57c0f1d2ceba0251b7a09b95d140be1
Ivan Kolle <kolleweb@gmail.com>
2018-02-23 02:14:56 +0300

    v.0.2

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
 }
 
-export default App;
+const user = {
+  firstName: "Ivan",
+  lastName: "Kolle"
+};
+
+const element = (
+  <h1>
+    Hello {formatName(user)}!
+  </h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
+export {element};
